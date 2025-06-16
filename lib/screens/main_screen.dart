9ad75c3e3db2a3caed4a13a4278b03e421592b6c
김경원 <seoulorigin@seoulorigin-MacBookAir.local>
2025-06-16 15:29:44 +0900

    질문 내역 불러오기 수정

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [],
+        ),
+      ),
+    );
+  }
+} 
